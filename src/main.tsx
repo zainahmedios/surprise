b2bbdfe5ccbdff7331394cd56c33b90f1c1da09b
Zain Ahmed <44906047+zainahmedios@users.noreply.github.com>
2025-09-17 19:49:01 -0500

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import MobileSlideDemo from "./MobileSlideDemo";
+
+createRoot(document.getElementById("root")!).render(<MobileSlideDemo />);
